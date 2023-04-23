2177b1a94ccf22dbc4110a8b84d8c77e5719c8a5
Abel Tennison-Riley <attennisonriley@gmail.com>
2023-04-23 16:22:50 +0100

    add basic functions

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function add(a, b){
+    return a + b;
+}
+
+function mutiply(a,b){
+    return a * b;
+}
+
+function divive(a,b){
+    return a/b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
