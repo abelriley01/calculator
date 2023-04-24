f2c6972f01e4b1a8caf1d3441166b4830a525c67
Abel Tennison-Riley <attennisonriley@gmail.com>
2023-04-24 21:38:23 +0100

    add more functions to help with calculations

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,50 @@
+let firstNumber;
+let secondNumber;
+let operator;
+let currentNumber ="";
+let displayValue = document.getElementById("currentNumber");
+let runningDisplay = document.getElementById("runningDisplay");
+const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+const calculate = document.querySelector("#equals");
+
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const numberValue = button.innerText;
+    if (!isOperator(numberValue)) {
+    currentNumber += value;
+    updateDisplay(currentNumber);
+    }
+  });
+});
+
+function updateDisplay(value){
+    displayValue.innerText = value;
+}
+
+function updateRunningCalculation(calc){
+    runningDisplay.innerText = calc;
+}
+function isOperator(value) {
+    return value === "+" || value === "-" || value === "*" || value === "/";
+}
+
+function operate(num1,sign,num2){
+    if (sign === '+'){
+        add(num1, num2);
+    }
+    else if (sign === '-'){
+        subtract(num1, num2);
+    }
+    else if (sign === '/'){
+        divide(num1, num2);
+    }
+    else if (sign === '*'){
+        multiply(num1, num2);
+    }
+}
+
 function add(a, b){
     return a + b;
 }
@@ -6,7 +53,7 @@ function mutiply(a,b){
     return a * b;
 }
 
-function divive(a,b){
+function divide(a,b){
     return a/b;
 }
 
